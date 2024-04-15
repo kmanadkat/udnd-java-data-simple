b23f7d69e823a5bcf2dd9283cc325bb241582c73
Krupesh Anadkat <kmanadkat@gmail.com>
2024-04-15 12:20:27 +0530

    init: Spring Boot Project Starter

---
+++ src/main/java/com/krupeshanadkat/jdnddatapersistence/JdndDataPersistenceApplication.java
@@ -0,0 +1,13 @@
+package com.krupeshanadkat.jdnddatapersistence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdndDataPersistenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdndDataPersistenceApplication.class, args);
+	}
+
+}
