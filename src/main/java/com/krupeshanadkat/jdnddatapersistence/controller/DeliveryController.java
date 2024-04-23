2083958c04e953605f66aa763778eb25c24f31ca
Krupesh Anadkat <kmanadkat@gmail.com>
2024-04-23 11:44:44 +0530

    feat: Added Repository Pattern

--- src/main/java/com/krupeshanadkat/jdnddatapersistence/controller/DeliveryController.java
+++ src/main/java/com/krupeshanadkat/jdnddatapersistence/controller/DeliveryController.java
@@ -1,17 +1,27 @@
 package com.krupeshanadkat.jdnddatapersistence.controller;
 
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.krupeshanadkat.jdnddatapersistence.data.Delivery;
+import com.krupeshanadkat.jdnddatapersistence.data.Plant;
 import com.krupeshanadkat.jdnddatapersistence.data.RecipientAndPrice;
 import com.krupeshanadkat.jdnddatapersistence.service.DeliveryService;
+import com.krupeshanadkat.jdnddatapersistence.service.PlantService;
+import com.krupeshanadkat.jdnddatapersistence.views.Views;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/delivery")
 public class DeliveryController {
     @Autowired
     DeliveryService deliveryService;
+
+    @Autowired
+    PlantService plantService;
 
     @PostMapping
     public Long scheduleDelivery(@RequestBody Delivery delivery) {
@@ -21,5 +31,16 @@ public class DeliveryController {
     @GetMapping("/bill/{deliveryId}")
     public RecipientAndPrice getBill(@PathVariable Long deliveryId) {
         return deliveryService.getBill(deliveryId);
+    }
+
+    @GetMapping("/delivered/{id}")
+    public Boolean delivered(@PathVariable Long id) {
+        return plantService.delivered(id);
+    }
+
+    @GetMapping("/under-price/{price}")
+    @JsonView(Views.Public.class)
+    public List<Plant> plantsCheaperThan(@PathVariable BigDecimal price) {
+        return plantService.findPlantsBelowPrice(price);
     }
 }
