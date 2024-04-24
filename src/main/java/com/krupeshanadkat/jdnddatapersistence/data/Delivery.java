579c731f6b54d5b9b6556aff8f03ab852cd6a046
Krupesh Anadkat <kmanadkat@gmail.com>
2024-04-24 18:26:46 +0530

    test: Unit Testing

--- src/main/java/com/krupeshanadkat/jdnddatapersistence/data/Delivery.java
+++ src/main/java/com/krupeshanadkat/jdnddatapersistence/data/Delivery.java
@@ -26,6 +26,14 @@ public class Delivery {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "delivery", cascade = CascadeType.ALL)
     private List<Plant> plants;
 
+    public Delivery(String name, String address, LocalDateTime deliveryTime) {
+        this.name = name;
+        this.address = address;
+        this.deliveryTime = deliveryTime;
+    }
+
+    public Delivery() {}
+
     public Long getId() {
         return id;
     }
--- src/main/java/com/krupeshanadkat/jdnddatapersistence/data/Plant.java
+++ src/main/java/com/krupeshanadkat/jdnddatapersistence/data/Plant.java
@@ -26,6 +26,13 @@ public class Plant {
     @JoinColumn(name = "delivery_id")  //map the join column in the plant table
     private Delivery delivery;
 
+    public Plant(String name, double price){
+        this.name = name;
+        this.price = BigDecimal.valueOf(price);
+    }
+
+    public Plant() {}
+
     public Long getId() {
         return id;
     }
